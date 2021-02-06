4b586a0650c3bd5add93a4a75325b354d3fbf5b4
mr904i <hashimoto_keisuke@hashi-MBP.local>
2021-02-06 15:59:50 +0900

    implements user authentication form view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
+  # user authentication
+  resources :sessions, only: %i[index]
+  get    '/sign_in', to: 'sessions#new'
+  post   '/sign_in', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
