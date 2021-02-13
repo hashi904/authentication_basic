5c0e355768870baf0a47ee8c099bea3fb4138aa8
mr904i <hashimoto_keisuke@hashi-MBP.local>
2021-02-13 09:50:30 +0900

    implements form object

---
+++ app/forms/form_user_new.rb
@@ -0,0 +1,18 @@
+class FormUserNew
+  include ActiveModel::Model
+
+  attr_accessor :nickname, :email, :password
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  def to_model
+    User.new(nickname: nickname, email: email, password: password)
+  end
+
+  def save
+    return false if invalid?
+    to_model.save
+  end
+end
