ab4dfed2c86cb85fc9a07a287ec02390bd5e464f
mr904i <hashimoto_keisuke@hashi-MBP.local>
2021-02-18 09:28:10 +0900

    implements sign in spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 describe SessionsController, type: :controller do
+  let(:user) { create(:user) }
+
   describe 'GET #new' do
     it 'http status 200' do
       get :new
       expect(response.status).to eq 200
+    end
+  end
+
+  describe 'GET #create' do
+    let!(:new_session_params) { { email: email, password: password } }
+
+    context 'ログインに成功する時' do
+      let!(:email) { 'test@gmail.com' }
+      let!(:password) { 'testtest' }
+
+      it 'http status 200' do
+        post :create, params: { new_session_form: new_session_params }
+        expect(response.status).to eq 200
+      end
     end
   end
 end
