86cdd3bf8499832490ebb88a53380dcd2c83eea4
mr904i <hashimoto_keisuke@hashi-MBP.local>
2021-02-22 09:09:03 +0900

    imoplements issue jwt

--- app/controllers/concerns/jwt_authentication.rb
+++ app/controllers/concerns/jwt_authentication.rb
@@ -1,3 +1,14 @@
 module JwtAuthentication
   extend ActiveSupport::Concern
+
+  def create_jwt(preload_user)
+    Authentication::JwtAuthenticator.encode(preload_user)
+  end
+
+  # todo 有効チェック
+  def check_jwt(token)
+    Authentication::JwtAuthenticator.decode(token)
+  end
+
+  # 例外処理
 end
