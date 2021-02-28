7a9e92a7fef86999a6a7f21d54e5841fb1b1232e
mr904i <hashimoto_keisuke@hashi-MBP.local>
2021-02-28 22:21:58 +0900

    add api sign up error message

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -41,4 +41,38 @@ describe Api::V1::UsersController, type: :controller do
       end
     end
   end
+
+  describe 'POST #sign_up' do
+    let!(:sign_up_params) do
+      {
+        nickname: nickname,
+        email: email,
+        password: password
+      }
+    end
+
+    context 'sigin upに成功する時' do
+      let!(:nickname) { 'sign_in_user' }
+      let!(:email) { 'sign_in_user@gmail.com' }
+      let!(:password) { 'sign_in_user_pass' }
+
+      it 'return http response 200' do
+        post :sign_up, params: sign_up_params, xhr: true
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'sigin upに失敗する時' do
+      context 'nicknameが空の時' do
+        let!(:nickname) { nil }
+        let!(:email) { 'sign_in_user@gmail.com' }
+        let!(:password) { 'sign_in_user_pass' }
+
+        it 'return http response 401' do
+          post :sign_up, params: sign_up_params, xhr: true
+          expect(response.status).to eq 401
+        end
+      end
+    end
+  end
 end
